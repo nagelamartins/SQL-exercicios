ac9b4d492449cd83f7f5cfc04551b20ff71e7f75
Nágela Martins <nagela.msouza@gmail.com>
2025-06-20 16:40:32 -0300

    Update schema.sql
    Inseridos os comandos SQL para criação do banco de dados e tabelas.

--- schema.sql
+++ schema.sql
@@ -1,1 +1,21 @@
+CREATE DATABASE musica; 
 
+USE musica;
+
+CREATE TABLE bandas (
+  id INT AUTO_INCREMENT NOT NULL,
+  nome VARCHAR(100) UNIQUE,
+  genero VARCHAR(100),
+  ano YEAR,
+  nacionalidade VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE albuns (
+  id INT AUTO_INCREMENT NOT NULL,
+  titulo VARCHAR(100),
+  ano YEAR,
+  banda_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (banda_id) REFERENCES bandas(id)
+);
