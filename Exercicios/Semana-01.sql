f8b77444e82712fcb5059c323f0f278a04780b46
Nágela Martins <nagela.msouza@gmail.com>
2025-06-23 19:17:01 -0300

    Create Semana-01.sql
    Inseri exercícios da semana 1, ainda sem respostas.

---
+++ Exercicios/Semana-01.sql
@@ -0,0 +1,27 @@
+/* 
+Objetivos da Semana 1:
+- Realizar consultas básicas com `SELECT`, `WHERE`, `DISTINCT` e `ORDER BY`
+- Inserir e atualizar dados com `INSERT` e `UPDATE`
+
+Data: 
+*/
+
+-- 1. Liste o nome de todas as bandas cadastradas.
+
+-- 2. Liste os títulos e anos de todos os álbuns.
+
+-- 3. Mostre o nome das bandas brasileiras.
+
+-- 4. Mostre o nome das bandas fundadas após 1990.
+
+-- 5. Liste os álbuns lançados antes de 1980.
+
+-- 6. Quais são os gêneros musicais únicos cadastrados na tabela bandas?
+
+-- 7. Liste o nome das bandas e o ano de fundação, ordenados do mais antigo para o mais recente.
+
+-- 8. Insira uma nova banda fictícia: "The Galactic Cats", do gênero "Space Rock", fundada em 2025, nacionalidade "Marte".
+
+-- 9. Insira um álbum dessa banda chamado "Cosmic Whiskers", lançado em 2025.
+
+-- 10. Atualize a nacionalidade da banda “The Galactic Cats” para "Fronteira Interestelar".
