600559b516b93a793a92078bb3410b318b2451e5
Nágela Martins <nagela.msouza@gmail.com>
2025-06-30 20:43:35 -0300

    Create Semana-02.sql
    Inserção dos exercícios da semana 2

---
+++ Exercicios/Semana-02.sql
@@ -0,0 +1,30 @@
+/* 
+Objetivos da Semana 2:
+- Praticar `JOIN` entre tabelas relacionadas.
+- Utilizar funções agregadas como `COUNT()`, `AVG()`, `MIN()`, `MAX()`.
+- Trabalhar com `GROUP BY` e `ORDER BY`.
+- Aplicar comandos `DDL` como `ALTER TABLE`.
+- Reforçar boas práticas ao manipular dados com segurança.
+
+Data: 
+*/
+
+-- 1. Liste o nome da banda e o título de cada álbum.
+
+-- 2. Liste apenas os álbuns da banda "Linkin Park" com título e ano.
+
+-- 3. Liste todas as bandas e quantos álbuns cada uma possui.
+
+-- 4. Quais são os 5 gêneros com mais bandas cadastradas?
+
+-- 5. Mostre a média de ano de fundação das bandas brasileiras.
+
+-- 6. Quais são os álbuns mais antigos e mais recentes do banco de dados?
+
+-- 7. Liste as bandas que não possuem nenhum álbum cadastrado.
+
+-- 8. Adicione uma nova coluna chamada site_oficial na tabela bandas, do tipo VARCHAR(150).
+
+-- 9. Atualize o site da banda "Linkin Park" para 'https://www.linkinpark.com'.
+
+-- 10. Exclua a banda fictícia "The Galactic Cats" e seus álbuns associados.
