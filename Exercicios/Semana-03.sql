5efed420994f25d6c8221a7c1ef64d603f86e2b9
Nágela Martins <nagela.msouza@gmail.com>
2025-07-07 19:46:20 -0300

    Create Semana-03.sql
    inseridos os exercícios da semana 3

---
+++ Exercicios/Semana-03.sql
@@ -0,0 +1,27 @@
+/*
+Objetivo da Semana 3:
+- Praticar os diferentes tipos de JOIN.
+
+Observação: foram adicionadas novas bandas sem álbuns associados a elas, para fins de prática com JOINs.
+Data:
+*/
+
+-- 1. Liste todas as bandas e seus respectivos álbuns.
+
+-- 2. Liste todas as bandas, mesmo as que não têm nenhum álbum cadastrado.
+
+-- 3. Liste os nomes das bandas e o número total de álbuns que contêm mais de 20 caracteres no título.
+
+-- 4. Simule um FULL OUTER JOIN entre bandas e álbuns.
+
+-- 5. Liste os álbuns lançados após 2000, junto com o nome da banda correspondente.
+
+-- 6. Liste os gêneros musicais únicos de bandas que possuem pelo menos 3 álbuns.
+
+-- 7. Mostre as bandas que têm álbuns com mais de uma palavra no título.
+
+-- 8. Liste os nomes das bandas e seus álbuns que contêm a palavra "side" no título.
+
+-- 9. Mostre os nomes das bandas fundadas antes de 1980 que ainda têm álbuns cadastrados no banco.
+
+-- 10. Liste a quantidade total de álbuns por nacionalidade das bandas, incluindo aquelas sem nenhum álbum (use LEFT JOIN com GROUP BY).
