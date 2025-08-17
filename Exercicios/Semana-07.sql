6b577e37fa6081874d20775e6ad9a9da9b0e08dd
Nágela Martins <nagela.msouza@gmail.com>
2025-08-16 22:44:44 -0300

    Create Semana-07.sql
    Inseridos os exercícios da semana 7.

---
+++ Exercicios/Semana-07.sql
@@ -0,0 +1,27 @@
+/*
+Objetivo da Semana 7:
+- Praticar DDL: criar, alterar e adicionar colunas em tabelas;
+- Praticar DML: inserir, atualizar e excluir dados. 
+
+Data: 
+*/
+
+-- 1. Crie uma nova tabela chamada integrantes com os campos "nome", "instrumento", "membro atual (Sim ou Não)".
+
+-- 2. Atualize a coluna site da tabela bandas inserindo ou corrigindo o endereço dos sites oficiais de pelo menos 3 bandas.
+
+-- 3. Insira 3 registros na tabela integrantes, cada um associado a uma banda diferente.
+
+-- 4. Atualize um integrante da tabela integrantes, alterando o instrumento tocado.
+
+-- 5. Exclua 1 integrante da tabela integrantes.
+
+-- 6. Liste todos os integrantes e suas respectivas bandas.
+
+-- 7. Atualize o campo site da tabela bandas de pelo menos 2 bandas.
+
+-- 8. Insira uma nova banda completa em bandas.
+
+-- 9. Altere a tabela integrantes para incluir uma nova coluna chamada data_nascimento, do tipo DATE.
+
+-- 10. Insira a data de nascimento dos integrantes. 
