12e3239a44f31a54bb34ec5b07fa5acf3ec3dc45
Nágela Martins <nagela.msouza@gmail.com>
2025-08-20 22:56:29 -0300

    Create Semana-08.sql
    Inseridos os exercícios da semana 8.

---
+++ Exercicios/Semana-08.sql
@@ -0,0 +1,28 @@
+/*
+Objetivo da Semana 7:
+- Praticar conhecimentos em JOIN;
+- Aprimorar o uso de subqueries e funções de agregação;
+- Trabalhar com a nova tabela integrantes para extrair informações.
+
+Data: 
+*/
+
+-- 1. Liste o nome das bandas e o nome de todos os seus integrantes. Inclua as bandas que não têm nenhum integrante cadastrado.
+
+-- 2. Mostre o nome dos integrantes que nasceram antes de 1970.
+
+-- 3. Conte quantos integrantes cada banda possui. Inclua apenas as bandas que têm pelo menos 2 integrantes.
+
+-- 4. Liste o nome dos integrantes que são membros S (atuais) e o nome da banda a que pertencem.
+
+-- 5. Encontre as bandas que têm integrantes cujos nomes começam com a letra 'J'.
+
+-- 6. Mostre o nome da banda e o número total de álbuns, apenas para as bandas de rock (Rock).
+
+-- 7. Liste o nome dos integrantes e o ano de formação de sua respectiva banda.
+
+-- 8. Encontre as bandas que têm álbuns e que têm integrantes cadastrados, usando um INNER JOIN entre as três tabelas (bandas, albuns e integrantes).
+
+-- 9. Liste os integrantes que pertencem a bandas que lançaram álbuns antes de 1980.
+
+-- 10. Insira 3 novos álbuns para a banda U2 na tabela albuns.
