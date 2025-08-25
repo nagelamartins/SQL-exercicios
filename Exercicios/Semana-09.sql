c49971444daff30a3545809c20b3256b8f884f82
Nágela Martins <nagela.msouza@gmail.com>
2025-08-25 13:57:46 -0300

    Create Semana-09.sql
    Inseridos os exercícios da semana 9.

---
+++ Exercicios/Semana-09.sql
@@ -0,0 +1,26 @@
+/*
+Objetivo da Semana 9:
+- Praticar o uso de GROUP BY com HAVING e INNER JOIN em múltiplas tabelas.
+
+Data:
+*/
+
+-- 1. Conte quantos integrantes nasceram em cada década (1960, 1970, 1980, etc.).
+
+-- 2. Liste os nomes das bandas com mais de 3 álbuns e que pertencem ao gênero "Heavy Metal".
+
+-- 3. Encontre os álbuns lançados no mesmo ano em que a banda foi formada. Liste o nome da banda e o título do álbum.
+
+-- 4. Mostre o nome de todas as bandas, o gênero musical e uma nova coluna chamada status que diga 'Clássica' se a banda foi fundada antes de 1980 e 'Moderna' se foi fundada em 1980 ou depois.
+
+-- 5. Liste os nomes dos integrantes que não são membros atuais (membro_atual = 'N').
+
+-- 6. Calcule a média de idade dos integrantes que são membros atuais (membro_atual = 'S').
+
+-- 7. Encontre as bandas que têm pelo menos um integrante com o mesmo ano de nascimento que o ano de lançamento de um de seus álbuns.
+
+-- 8. Liste a quantidade de álbuns por gênero. Inclua apenas os gêneros que têm mais de 5 álbuns.
+
+-- 9. Mostre o nome das bandas, o título de seus álbuns e o ano de formação da banda, para todas as bandas do Brasil.
+
+-- 10. Exclua a banda U2 e seus álbuns e integrantes associados.
