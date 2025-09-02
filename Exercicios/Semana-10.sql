5054ae84e3e5ec1bd63282b9af18afb8354c573c
Nágela Martins <nagela.msouza@gmail.com>
2025-09-02 20:22:14 -0300

    Create Semana-10.sql
    Inseridos os exercícios da semana 10.

---
+++ Exercicios/Semana-10.sql
@@ -0,0 +1,27 @@
+/*
+Objetivos da Semana 10:
+- Praticar o uso de subqueries aninhadas;
+- Inserir o uso de funções de janela
+
+Data: 
+*/
+
+-- 1. Mostre o nome da banda, o título do álbum e o ano de formação, para todas as bandas que lançaram álbuns após o ano 2000.
+
+-- 2. Encontre as bandas que têm mais álbuns do que a média de álbuns por banda.
+
+-- 3. Liste o nome dos integrantes que nasceram antes do ano de fundação de sua respectiva banda.
+
+-- 4. Exiba o nome da banda, o título do álbum e a posição do álbum em relação à data de lançamento (1 para o mais antigo, 2 para o segundo mais antigo, etc.), agrupado por banda.
+
+-- 5. Conte quantos integrantes cada instrumento possui e liste apenas os instrumentos que têm mais de 2 integrantes.
+
+-- 6. Liste os álbuns que têm o mesmo ano de lançamento de pelo menos um álbum da banda "Black Sabbath".
+
+-- 7. Encontre os nomes das bandas que têm pelo menos um integrante cujo nome contém a letra 'o' e um álbum com o ano de lançamento entre 1990 e 2000.
+
+-- 8. Exclua todos os álbuns da banda The Beatles.
+
+-- 9. Atualize o gênero da banda Ramones para 'Punk'.
+
+-- 10. Liste a quantidade de álbuns de cada nacionalidade, incluindo aquelas que não têm nenhum álbum associado.
