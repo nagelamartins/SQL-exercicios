80b08db9ac4651496d3d3948c3f1429d64507744
Nágela Martins <nagela.msouza@gmail.com>
2025-11-02 13:03:25 -0300

    Update Semana-13.sql
    conclusão dos exercícios da semana 13.

--- Exercicios/Semana-13.sql
+++ Exercicios/Semana-13.sql
@@ -2,7 +2,7 @@
 Objetivo da Semana 12:
 - Praticar funções de manipulação de dados e agregações. 
 
-Datas: 07/10/2025 - em andamento
+Datas: 07/10/2025 - 02/11/2025
 */
 
 -- 1. Mostre o nome da banda e o número de álbuns, mas apenas para as 3 bandas com o maior número de álbuns.
@@ -38,23 +38,41 @@ JOIN integrantes AS i
 ON b.id = i.banda_id
 WHERE i.instrumento LIKE '%vocal%';
 
--- 5. Para cada banda, mostre o nome do álbum mais recente e o nome do seu álbum mais antigo, sem usar funções de janela ou subqueries correlacionadas.
-
--- 6. Liste os álbuns que foram lançados em anos pares.
+-- 5. Liste os álbuns que foram lançados em anos pares.
 SELECT titulo FROM albuns
 WHERE ano % 2 = 0;
 
--- 7. Mostre o nome de cada banda e o número de integrantes que ela possui, mas apenas para as bandas com 4 ou mais integrantes.
+-- 6. Mostre o nome de cada banda e o número de integrantes que ela possui, mas apenas para as bandas com 4 ou mais integrantes.
 SELECT b.nome AS banda, count(i.nome) AS qtdd_integrantes FROM bandas AS b
 JOIN integrantes AS i
 ON b.id = i.banda_id
 GROUP BY banda
 HAVING qtdd_integrantes >= 4;
 
--- 8. Encontre a quantidade de álbuns por gênero, mas exiba somente os gêneros com mais de 2 bandas.
+-- 7. Encontre a quantidade de álbuns por gênero, mas exiba somente os gêneros com mais de 2 bandas.
+SELECT b.genero, count(a.titulo) AS qtdd_albuns FROM bandas AS b
+JOIN albuns AS a
+ON b.id = a.banda_id
+GROUP BY b.genero
+HAVING qtdd_albuns > 2
+ORDER BY qtdd_albuns DESC;
 
--- 9. Liste os nomes dos integrantes, o nome da banda e uma coluna chamada status_membro que mostre 'Membro Ativo' se membro_atual for 'S' e 'Ex-Membro' se for 'N'.
+-- 8. Liste os nomes dos integrantes, o nome da banda e uma coluna chamada status_membro que mostre 'Membro Ativo' se membro_atual for 'S' e 'Ex-Membro' se for 'N'.
+SELECT b.nome AS banda, i.nome AS integrante, 
+	CASE 
+		WHEN membro_atual = 'S' THEN 'Membro Ativo'
+		ELSE 'Ex_membro'
+	END AS status_membro
+FROM bandas AS b
+JOIN integrantes AS i
+ON b.id = i.banda_id;
 
--- 10. Exclua todos os integrantes da banda The Beatles.
+-- 9. Exclua todos os integrantes da banda The Beatles.
 DELETE FROM integrantes
 WHERE banda_id = (SELECT id FROM bandas WHERE nome = 'The Beatles');
+
+-- 10. Liste o nome de todas as bandas que têm integrantes cujo data_nascimento é anterior a 1960.
+SELECT DISTINCT b.nome AS banda FROM bandas AS b
+JOIN integrantes AS i
+ON b.id = i.banda_id
+WHERE YEAR(i.data_nascimento) < 1960;
